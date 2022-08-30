b13f37d259da4dc1f692a22f2019335cc0d4ee39
Ivan Ermolaev <fnaferm@gmail.com>
2022-08-30 17:49:49 +0700

    [add] enemy ai horizontal movement

---
+++ Assets/Scripts/Enemy/EnemyAIBehavior.cs
@@ -0,0 +1,93 @@
+using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class EnemyAIBehavior : MonoBehaviour
+{
+    [SerializeField] private float nextWaypointDistance;
+
+    private Path path;
+    private EnemyAIVariables variables;
+    private Seeker seeker;
+
+    private IDisposable disposable;
+
+    private Transform target;
+    private Rigidbody2D rb;
+
+    private int currentWaypoint = 0;
+
+
+    public void InitBehavior()
+    {
+        variables = GetComponent<EnemyAIVariables>();
+        rb = GetComponent<Rigidbody2D>();
+        seeker = GetComponent<Seeker>();
+        
+        variables.InitVariables();
+
+        target = GlobalAI.Instance.Player.transform;
+    }
+
+    public void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(transform.position, target.position, OnGeneratePathComplete);
+        }
+    }
+
+    public void TurnOnPathUpdate()
+    {
+        disposable = Observable.Interval(TimeSpan.FromSeconds(0.5f)).Subscribe(_=>
+        {
+            UpdatePath();
+        });
+    }
+
+    public void TurnOffPathUpdate()
+    {
+        disposable.Dispose();
+    }
+
+    public bool IsPathHavePath()
+    {
+        return path != null;
+    }
+
+    private void OnGeneratePathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    public void NextWaypoint()
+    {
+        currentWaypoint++;
+    }
+
+    public bool IsReachedWaypoint()
+    {
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        return nextWaypointDistance > distance;
+    }
+
+    public bool IsReachedPath()
+    {
+        return currentWaypoint >= path.vectorPath.Count;
+    }
+
+    public void MoveToPlayer()
+    {
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * variables.speed * Time.fixedDeltaTime;
+
+        rb.AddForce(force);
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyAIVariables.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAIVariables : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    public void InitVariables()
+    {
+        
+    }
+}
